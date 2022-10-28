78ef9a3fa0ae2158a7f3be7124e59117ed893cc6
Elkin Jaramillo <elkin.dev22@gmail.com>
2022-10-27 21:31:37 -0500

    Commit

--- app/src/main/java/com/example/food_nearby/login_registro_v2.java
+++ app/src/main/java/com/example/food_nearby/login_registro_v2.java
@@ -1,14 +1,14 @@
 package com.example.food_nearby;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class login_registro_v2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_registro_v1);
+        setContentView(R.layout.login_registro_v2);
     }
 }
