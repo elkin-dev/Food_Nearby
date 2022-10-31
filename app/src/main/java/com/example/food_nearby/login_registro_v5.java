c281f5573b81220912092dc99ce3245ade6ac678
Elkin Jaramillo <elkin.dev22@gmail.com>
2022-10-31 10:01:49 -0500

    Terminando login registro

---
+++ app/src/main/java/com/example/food_nearby/login_registro_v5.java
@@ -0,0 +1,14 @@
+package com.example.food_nearby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_registro_v5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_registro_v5);
+    }
+}
